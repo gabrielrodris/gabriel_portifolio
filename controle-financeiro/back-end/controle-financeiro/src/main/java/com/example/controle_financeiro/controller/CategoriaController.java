e808dc00e986307afc7a4ca46a0555914c28fa3c
gabrielrodris <biel790832157@gmail.com>
2025-06-24 13:59:06 -0300

    controller Categoria

---
+++ controle-financeiro/back-end/controle-financeiro/src/main/java/com/example/controle_financeiro/controller/CategoriaController.java
@@ -0,0 +1,9 @@
+package com.example.controle_financeiro.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+}
